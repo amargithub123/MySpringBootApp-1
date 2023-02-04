49afba8b37d150d4278db65f4144e7cec833c9bd
amargithub123 <amarbhairat25@gmail.com>
2023-02-05 00:08:57 +0530

    Initial Commit

---
+++ src/main/java/com/ab/springboot/SpringBootNewAppApplication.java
@@ -0,0 +1,13 @@
+package com.ab.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNewAppApplication.class, args);
+	}
+
+}
